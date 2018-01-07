8a2bfa43101cc0d0fb9d5b2236d7756ee0fc3f97
Oleg Sokolansky <olegsokolansky@Olegs-MacBook-Pro.local>
2018-01-07 19:32:44 +0200

    Initial commit

---
+++ complete/src/main/java/hello/Character.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class Character {
+    public String name;
+    public Integer initiative;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getInitiative() {
+        return initiative;
+    }
+
+    public void setInitiative(Integer initiative) {
+        this.initiative = initiative;
+    }
+}
